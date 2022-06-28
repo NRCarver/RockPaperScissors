27396b59d230726fe6eaba905d43e814a9f267ac
NRCarver <carver1994@hotmail.com>
2022-06-28 19:05:10 +0000

    Add js function skeleton

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+function newGame () {
+
+}
+
+function selectRock () {
+
+
+}
+
+function selectPaper () {
+
+    
+}
+
+function selectScissors () {
+
+    
+}
+
+function selectLizard () {
+
+    
+}
+
+function selectSpock () {
+
+    
+}
+
+function computerChoice () {
+    
+}
+
+function endGame () {
+
+}
+
+function playerWin () {
+
+    
+}
+
+function computerWin () {
+
+    
+}
+
+function endGame () {
+
+    
+}
+
+function classicGame () {
+
+    
+}
