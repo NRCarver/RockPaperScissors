116cab11cd0a7e71a810621d803c222419af7eb2
NRCarver <carver1994@hotmail.com>
2022-06-28 19:28:43 +0000

    Add js for each button choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,31 +4,26 @@ function newGame () {
 
 function selectRock () {
 
-
 }
 
 function selectPaper () {
-
-    
+ 
 }
 
 function selectScissors () {
-
-    
+   
 }
 
 function selectLizard () {
-
-    
+  
 }
 
 function selectSpock () {
-
-    
+  
 }
 
 function computerChoice () {
-    
+
 }
 
 function endGame () {
@@ -36,21 +31,30 @@ function endGame () {
 }
 
 function playerWin () {
-
-    
+   
 }
 
 function computerWin () {
 
-    
 }
 
 function endGame () {
 
-    
 }
 
 function classicGame () {
+   
+}
 
-    
+const choiceButtons = document.querySelectorAll(`[data-choice]`)
+
+choiceButtons.forEach(choiceButton => {
+    choiceButton.addEventListener('click', e => {
+        const choiceName = choiceButton.dataset.choice
+        makeChoice(choiceName)
+    })
+})
+
+function makeChoice(choice) {
+    console.log(choice)
 }
