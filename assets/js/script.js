95f8e7244bb4d76718d2f7521d94dd885caff69d
NRCarver <carver1994@hotmail.com>
2022-06-30 01:27:23 +0000

    Add missing ; to js after using validator

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,34 +30,34 @@ const CHOICES = [
 
 
 
-const choiceButtons = document.querySelectorAll('[data-choice]')
-const resultsColumn = document.getElementById('results-column')
-const computerScore = document.getElementById('computer-score')
-const userScore = document.getElementById('user-score')
-let roundCount = 0
-let maxRounds = 5
-let roundCountOutput = document.getElementById('round-count')
+const choiceButtons = document.querySelectorAll('[data-choice]');
+const resultsColumn = document.getElementById('results-column');
+const computerScore = document.getElementById('computer-score');
+const userScore = document.getElementById('user-score');
+let roundCount = 0;
+let maxRounds = 5;
+let roundCountOutput = document.getElementById('round-count');
 
 choiceButtons.forEach(choiceButton => {
     choiceButton.addEventListener('click', e => {
-        const choiceName = choiceButton.dataset.choice
-        const choice = CHOICES.find(choice => choice.name === choiceName)
-        makeChoice(choice)
-    })
-})
+        const choiceName = choiceButton.dataset.choice;
+        const choice = CHOICES.find(choice => choice.name === choiceName);
+        makeChoice(choice);
+    });
+});
 
 /* Determine the Computers selection, who won the round and increasing the round counter and ending the game after 5 rounds */
 
 function makeChoice(choice) {
-    const computerChoice = computerIndex()
-    let userWinner = decideWinner(choice, computerChoice)
-    let computerWinner = decideWinner(computerChoice, choice)
+    const computerChoice = computerIndex();
+    let userWinner = decideWinner(choice, computerChoice);
+    let computerWinner = decideWinner(computerChoice, choice);
     
-    addChoiceResult(computerChoice, computerWinner)
-    addChoiceResult(choice, userWinner)
+    addChoiceResult(computerChoice, computerWinner);
+    addChoiceResult(choice, userWinner);
 
-    if (userWinner) incrementUserScore(userScore)
-    if (computerWinner) incrementComputerScore(computerScore)
+    if (userWinner) incrementUserScore(userScore);
+    if (computerWinner) incrementComputerScore(computerScore);
 
     roundCount++;
     roundCountOutput.innerText = `${roundCount}`;
@@ -65,7 +65,7 @@ function makeChoice(choice) {
      if (roundCount >= maxRounds) {
         playAgain.classList.remove('hidden');
         setTimeout(function() {
-            alert("Game Over. The score was " + userScore.innerText + "-" + computerScore.innerText)
+            alert("Game Over. The score was " + userScore.innerText + "-" + computerScore.innerText);
         }, 300);
 
         let disabledButtons = document.getElementsByClassName("choice");
@@ -79,56 +79,56 @@ function makeChoice(choice) {
 /* Increase the display scores for user and computer */
 
 function incrementUserScore(userScore) {
-    userScore.innerText = parseInt(userScore.innerText) + 1
+    userScore.innerText = parseInt(userScore.innerText) + 1;
 }
 
 function incrementComputerScore(computerScore) {
-    computerScore.innerText = parseInt(computerScore.innerText) + 1
+    computerScore.innerText = parseInt(computerScore.innerText) + 1;
 }
 
 /* Display what happened to the user, showing both selections and highlighting the winner */
 
 function addChoiceResult(choice, winner) {
-    const pastResults = document.createElement('div')
-    pastResults.innerText = choice.name
-    pastResults.classList.add('result-choice')
-    if (winner) pastResults.classList.add('winner')
-    else pastResults.classList.add('loser')
-    resultsColumn.after(pastResults)
+    const pastResults = document.createElement('div');
+    pastResults.innerText = choice.name;
+    pastResults.classList.add('result-choice');
+    if (winner) pastResults.classList.add('winner');
+    else pastResults.classList.add('loser');
+    resultsColumn.after(pastResults);
 }
 
 /* Logic to determine who wins */
 
 function decideWinner(choice, computerChoice){
     if (choice.beats == computerChoice.name) {
-        return choice.beats == computerChoice.name
+        return choice.beats == computerChoice.name;
     }
     if (choice.also == computerChoice.name) {
-        return choice.also == computerChoice.name
+        return choice.also == computerChoice.name;
     }   
 }
 
 /* Computers random selection */
 
 function computerIndex () {
-    const randomIndex = Math.floor(Math.random() * CHOICES.length)
-    return CHOICES[randomIndex]
+    const randomIndex = Math.floor(Math.random() * CHOICES.length);
+    return CHOICES[randomIndex];
 }
 
 
 
-const startClassicButton = document.getElementById('start-classic-button')
-const startRPSLSButton = document.getElementById('start-rpsls-button')
-const gameContainer = document.getElementById('game-container')
-const extraOptions = document.getElementById('rpsls-only')
-const gameLengthBox = document.getElementById('game-length-choices')
-const playAgain = document.getElementById('play-again')
+const startClassicButton = document.getElementById('start-classic-button');
+const startRPSLSButton = document.getElementById('start-rpsls-button');
+const gameContainer = document.getElementById('game-container');
+const extraOptions = document.getElementById('rpsls-only');
+const gameLengthBox = document.getElementById('game-length-choices');
+const playAgain = document.getElementById('play-again');
 
 /* Listener Events for starting each version of the Game */
 
 startClassicButton.addEventListener('click', startClassic);
 startRPSLSButton.addEventListener('click', startRPSLS);
-playAgain.addEventListener('click', resetGame)
+playAgain.addEventListener('click', resetGame);
 
 function startClassic() {
     startClassicButton.classList.add('hidden');
@@ -143,13 +143,11 @@ function startRPSLS() {
     startClassicButton.classList.add('hidden');
     startRPSLSButton.classList.add('hidden');
     gameContainer.classList.remove('hidden');
-    gameLengthBox.classList.remove('hidden')
+    gameLengthBox.classList.remove('hidden');
 }
 
 /* Reload the Game so you can play again */
 
 function resetGame() {
-    return window.location.assign(`index.html`)
+    return window.location.assign(`index.html`);
 }
-
-/*jshint esversion: 6 */
